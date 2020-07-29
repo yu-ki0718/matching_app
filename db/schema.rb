bfd1314eda1b486521266be15dec44976d7991f0
yu-ki0718 <silver_k_24@icloud.com>
2020-07-29 15:16:57 +0900

    added MatchingSaveFunction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_135621) do
+ActiveRecord::Schema.define(version: 2020_07_29_060037) do
+
+  create_table "reactions", force: :cascade do |t|
+    t.integer "to_user_id", null: false
+    t.integer "from_user_id", null: false
+    t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
