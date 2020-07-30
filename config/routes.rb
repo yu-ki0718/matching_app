0f20d76beab5c3647e5b5f225403069a05f9b9a4
yu-ki0718 <silver_k_24@icloud.com>
2020-07-30 22:00:38 +0900

    added ChatRoom

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: %i(index show)
   resources :reactions, only: %i(create)
   resources :matching, only: %i(index)
+  resources :chat, only: %i(create show)
 
 end
